5e3126d6035accd544d16aded700a27edda9316b
RocketRhubarb <andreas.rockert@gmail.com>
2020-02-08 20:48:00 +0100

    Refactor to remove nested functions

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 #File: guessinggame.sh
 
-function guessinggame {
-	local num_files=$(ls -A | wc -l)
-	local guess
+function ask_num_files() {
 
-	ask_num_files() {
-		echo 'How many files are in the current directory?'
-		read guess
-		
-		# input sanitation
-		re='^[0-9]+$'
-		if ! [[ $guess =~ $re ]]
-		then
-			echo 'Not a number, try again.'
-			ask_num_files
-		fi
-	}
-
-	ask_num_files
-
-	while [[ ! $guess -eq $num_files ]]
-	do
-		if [[ $guess -gt $num_files ]]
-		then
-			echo 'You guessed too many.'
-		else
-			echo 'You guessed too few.'
-		fi	
+	echo 'How many files are in the current directory?'
+	read guess
+	
+	# input sanitation
+	re='^[0-9]+$'
+	if ! [[ $guess =~ $re ]]
+	then
+		echo 'Not a number, try again.'
 		ask_num_files
-	done
-
-	echo 'You guess the correct number of files in this directory.'
-	echo 'There are' $num_files 'files in the directory.'
+	fi
 }
 
-guessinggame
+num_files=$(ls -A | wc -l)
+guess
+
+ask_num_files
+
+while [[ ! $guess -eq $num_files ]]
+do
+	if [[ $guess -gt $num_files ]]
+	then
+		echo 'You guessed too many.'
+	else
+		echo 'You guessed too few.'
+	fi	
+	ask_num_files
+done
+
+echo 'You guess the correct number of files in this directory.'
+echo 'There are' $num_files 'files in the directory.'
+
